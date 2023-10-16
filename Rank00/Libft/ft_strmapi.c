e5515f783b52cc0c25a4d73626a7a12fab2fcd03
IttoSanzzo <konohittos@gmail.com>
2023-10-15 23:11:54 -0300

    Libft - Realizando v1

--- Rank00/Libft/ft_strmapi.c
+++ Rank00/Libft/ft_strmapi.c
@@ -6,13 +6,24 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/10/15 21:58:52 by marcosv2	       #+#    #+#	      */
-/*   Updated: 2023/10/15 22:22:02 by marcosv2         ###   ########.fr       */
+/*   Updated: 2023/10/15 22:32:36 by marcosv2         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	unsigned int	i;
+	char			*dst;
 
+	if (!s || !f)
+		return (NULL);
+	dst = ft_strdup((char *)s);
+	if (!dst)
+		return (NULL);
+	i = -1;
+	while (dst[++i])
+		dst[i] = f(i, dst[i]);
+	return (dst);
 }

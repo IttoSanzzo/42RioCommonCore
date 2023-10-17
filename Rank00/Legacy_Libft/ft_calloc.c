ae8ff8e56ec532d11dd92ac637e9d16b12758673
IttoSanzzo <konohittos@gmail.com>
2023-10-17 20:33:49 -0300

    Libft - oh Legacy... 3

--- Rank00/Legacy_Libft/ft_calloc.c
+++ Rank00/Legacy_Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/10/15 19:00:07 by marcosv2	       #+#    #+#	      */
-/*   Updated: 2023/10/16 18:36:54 by marcosv2         ###   ########.fr       */
+/*   Updated: 2023/10/17 20:33:33 by marcosv2         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*dst;
 
-	if (count * size == 0 || count > 2147483647 / size)
-		return (NULL);
 	dst = malloc(count * size);
 	if (!dst)
 		return (NULL);

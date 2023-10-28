56fff97998a92ace19a85e0de247f61bf4ee3bb9
IttoSanzzo <konohittos@gmail.com>
2023-10-27 23:47:09 -0300

    Libft ALL RIGHT 1

--- Rank00/Libft/ft_substr.c
+++ Rank00/Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/10/27 20:47:45 by marcosv2	       #+#    #+#	      */
-/*   Updated: 2023/10/27 20:50:02 by marcosv2         ###   ########.fr       */
+/*   Updated: 2023/10/27 23:46:54 by marcosv2         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*substr;
-	size_t	i;
+	char	*str;
 
 	if (!s)
-		return (0);
-	if (start >= ft_strlen(s))
-		return (ft_strdup(""));
+		return (NULL);
+	if (ft_strlen(s) < start)
+	{
+		str = (char *)malloc(sizeof(*s));
+		str[0] = '\0';
+		return (str);
+	}
+	if (ft_strlen(s) < start)
+		len = 0;
 	if (ft_strlen(s + start) < len)
 		len = ft_strlen(s + start);
-	substr = (char *)malloc(sizeof(char) * len + 1);
-	if (!substr)
-		return (0);
-	i = 0;
-	while (s[start] && i < len)
+	str = (char *)malloc(len + 1);
+	if (!str)
 	{
-		substr[i] = s[start];
-		i++;
-		start++;
+		return (NULL);
 	}
-	substr[i] = '\0';
-	return (substr);
+	ft_strlcpy(str, s + start, len + 1);
+	return (str);
 }

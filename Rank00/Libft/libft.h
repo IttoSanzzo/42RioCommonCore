a55bdf6339c5b6e924fbd25806dab7ef8fcb4df4
IttoSanzzo <konohittos@gmail.com>
2023-10-28 00:06:34 -0300

    Libft ALL RIGHT 4

--- Rank00/Libft/libft.h
+++ Rank00/Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/10/27 19:08:14 by marcosv2	       #+#    #+#	      */
-/*   Updated: 2023/10/27 21:04:08 by marcosv2         ###   ########.fr       */
+/*   Updated: 2023/10/28 00:06:11 by marcosv2         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *str, int c, size_t n);
 // ft_basic_four
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *src);

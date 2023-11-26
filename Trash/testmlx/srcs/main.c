03d3fd1560920586c3746ff51a4430a8f686bc83
IttoSanzzo <konohittos@gmail.com>
2023-11-26 19:17:46 -0300

    Trash

--- Trash/testmlx/srcs/main.c
+++ Trash/testmlx/srcs/main.c
@@ -6,19 +6,20 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/11/23 19:04:28 by marcosv2	       #+#    #+#	      */
-/*   Updated: 2023/11/23 19:52:32 by marcosv2         ###   ########.fr       */
+/*   Updated: 2023/11/24 19:14:53 by marcosv2         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
 #include "mlx.h"
-#include <stdio.h>
+//#include "../minilibx-linux/mlx.h"
 
 int	main(void)
 {
 	void	*mlx;
+//	void	*mlx_win;
 
 	mlx = mlx_init();
-
-	printf("Hello World!");	
+//	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
 	return (0);
 }

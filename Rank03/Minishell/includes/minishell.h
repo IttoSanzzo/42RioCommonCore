a3000f9085dda379a837825ebca71194fca808e1
marcosv2 <konohittos@gmail.com>
2023-12-04 13:50:10 -0300

    Minitalk | .h

--- Rank03/Minishell/includes/minishell.h
+++ Rank03/Minishell/includes/minishell.h
@@ -1,18 +1,31 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
+/*									      */
+/*							  :::	   ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gupiment <gupiment@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/04 13:38:17 by gupiment          #+#    #+#             */
-/*   Updated: 2023/12/04 13:38:47 by gupiment         ###   ########.fr       */
-/*                                                                            */
+/*						      +:+ +:+	      +:+     */
+/*   By: gupiment <gupiment@student.42.fr>	    +#+  +:+	   +#+	      */
+/*						  +#+#+#+#+#+	+#+	      */
+/*   Created: 2023/12/04 13:38:17 by gupiment	       #+#    #+#	      */
+/*   Updated: 2023/12/04 13:50:03 by marcosv2         ###   ########.fr       */
+/*									      */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+// Includes
 # include "libft.h"
 # include <stdio.h>
 # include <stdlib.h>
+
+// Pre-processors
+
+// Structs
+
+//// Functions per Archive
+// main.c
+int		main(int argc, **argc, char **envp);
+
+// ft_utils.c
+
 #endif

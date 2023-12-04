931ed62d1cae4ec8536224fb508e1a0e36c0a404
marcosv2 <konohittos@gmail.com>
2023-12-04 15:07:20 -0300

    Minitalk | start

--- Rank03/Minishell/includes/minishell.h
+++ Rank03/Minishell/includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: gupiment <gupiment@student.42.fr>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/12/04 13:38:17 by gupiment	       #+#    #+#	      */
-/*   Updated: 2023/12/04 13:50:46 by marcosv2         ###   ########.fr       */
+/*   Updated: 2023/12/04 14:54:05 by marcosv2         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
@@ -14,18 +14,24 @@
 # define MINISHELL_H
 
 // Includes
-# include "libft.h"
 # include <stdio.h>
 # include <stdlib.h>
+# include <unistd.h>
+# include "../libft/libft.h"
 
 // Pre-processors
+# define EXT_SUC 0
 
 // Structs
 
 //// Functions per Archive
 // main.c
-int		main(int argc, **argv, char **envp);
+int		main(int argc, char **argv, char **envp);
 
 // ft_utils.c
+
+///Builtins
+// exit.c
+void	ms_exit(void);
 
 #endif
--- Rank03/Minishell/src/main.c
+++ Rank03/Minishell/src/main.c
@@ -1,17 +1,23 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
+/*									      */
+/*							  :::	   ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gupiment <gupiment@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/04 13:36:45 by gupiment          #+#    #+#             */
-/*   Updated: 2023/12/04 13:37:42 by gupiment         ###   ########.fr       */
-/*                                                                            */
+/*						      +:+ +:+	      +:+     */
+/*   By: gupiment <gupiment@student.42.fr>	    +#+  +:+	   +#+	      */
+/*						  +#+#+#+#+#+	+#+	      */
+/*   Created: 2023/12/04 13:36:45 by gupiment	       #+#    #+#	      */
+/*   Updated: 2023/12/04 14:55:13 by marcosv2         ###   ########.fr       */
+/*									      */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **envp)
 {
+	(void) argc;
+	(void) argv;
+	(void) envp;
+
+	printf("Hello World!\n");
+	ms_exit();
 }

1b636c4635c457494fb373f72e6220ec10e66a25
marcosv2 <konohittos@gmail.com>
2023-12-12 16:45:34 -0300

    libft | ft_rejoin

--- libft/src/ft_rejoin.c
+++ libft/src/ft_rejoin.c
@@ -6,7 +6,7 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/12/12 16:32:12 by marcosv2	       #+#    #+#	      */
-/*   Updated: 2023/12/12 16:33:52 by marcosv2         ###   ########.fr       */
+/*   Updated: 2023/12/12 16:45:13 by marcosv2         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ char	*ft_rejoin(char	*s1, char *s2)
 	char	*rejoin;
 
 	rejoin = ft_strjoin(s1, s2);
-	free(s1);
+	if (s1)
+		free(s1);
 	return (rejoin);
 }

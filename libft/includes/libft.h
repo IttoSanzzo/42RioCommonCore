3f8e150d6e98ee6804d6fc1bdaf867e7e729c630
marcosv2 <konohittos@gmail.com>
2023-12-12 16:38:14 -0300

    libft | ft_rejoin

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/10/27 19:08:14 by marcosv2	       #+#    #+#	      */
-/*   Updated: 2023/12/08 09:16:40 by marcosv2         ###   ########.fr       */
+/*   Updated: 2023/12/12 16:37:38 by marcosv2         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
@@ -99,6 +99,9 @@ int		ft_nbrlen(int nb);
 //// Personal
 // utils
 void	ft_putstr(char *s);
+void	*ft_memdel(void *ptr);
+char	*ft_rejoin(char *s1, char *s2);
+
 
 // ft_readline.c
 char	*ft_readline(char *prompt);

e8b1104bab25e8475b6b9adb2919cb2e10935179
IttoSanzzo <konohittos@gmail.com>
2024-01-08 07:43:46 -0300

    minishell | SIGINT DONE

--- Rank03/Minishell/srcs/main/ms_parse.c
+++ Rank03/Minishell/srcs/main/ms_parse.c
@@ -6,13 +6,13 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2024/01/08 05:36:42 by marcosv2	       #+#    #+#	      */
-/*   Updated: 2024/01/08 07:36:40 by marcosv2         ###   ########.fr       */
+/*   Updated: 2024/01/08 07:42:08 by marcosv2         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ms_parse(t_mini *ms)
+static void	ms_readline(t_mini *ms)
 {
 	char	*su;
 
@@ -26,15 +26,19 @@ void	ms_parse(t_mini *ms)
 		ms_builtin_exit(ms);
 	}
 	if (ms->sig.sint || !ms->line[0])
-		return (ms_parse(ms));
+		return (ms_readline(ms));
 	while (!ft_cquotes(ms->line))
 	{
 		su = ft_readline("> ");
 		if (ms->line)
 			ft_freejoin(&ms->line, &su);
 		if (ms->sig.sint)
-			return (ms_parse(ms));
+			return (ms_readline(ms));
 	}
-	ft_printc(C_CYAN, "String..: %s\n", ms->line);
 	ft_add_history(ms->line);
+}
+
+void	ms_parse(t_mini *ms)
+{
+	ms_readline(ms);
 }
--- Rank03/Minishell/srcs/main/signal.c
+++ Rank03/Minishell/srcs/main/signal.c
@@ -6,7 +6,7 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/12/08 14:26:21 by marcosv2	       #+#    #+#	      */
-/*   Updated: 2024/01/08 07:30:04 by marcosv2         ###   ########.fr       */
+/*   Updated: 2024/01/08 07:43:18 by marcosv2         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
@@ -27,16 +27,13 @@ void	ms_sigint(int signum)
 
 	(void)signum;
 	ms = ms_get_mini(NULL);
-	if (ms->sig.pid == 0)
+	if (ms->sig.pid != 0)
 	{
-//		ft_putstr_fd("\b\b  ", STDERR);
-//		ft_putstr_fd("\n", STDERR);
-		ft_ungetchar('\n');
 		ms->sig.exit_status = 1;
 	}
 	else
 	{
-		ft_putstr_fd("\n", STDERR);
+		ft_ungetchar('\n');
 		ms->sig.exit_status = 130;
 	}
 	ms->sig.sint = 1;

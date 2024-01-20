4dc64cbd1a5f22790369d64cabd56d6913a17ecf
IttoSanzzo <konohittos@gmail.com>
2024-01-19 22:35:18 -0300

    Philosophers | Additions

--- Rank03/Philosophers/includes/philo.h
+++ Rank03/Philosophers/includes/philo.h
@@ -6,11 +6,19 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 22:01:56 by marcosv2          #+#    #+#             */
-/*   Updated: 2024/01/19 22:20:09 by marcosv2         ###   ########.fr       */
+/*   Updated: 2024/01/19 22:28:58 by marcosv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H 
+
+// Macros
+# include <stdlib.h>
+# include <unistd.h>
 # include <stdio.h>
+# include <string.h>
+# include <sys/time.h>
+# include <pthread.h>
+
 #endif

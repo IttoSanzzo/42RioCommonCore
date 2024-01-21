2748ab77a257cbff8b7f84aabceb7b984d1a7597
IttoSanzzo <konohittos@gmail.com>
2024-01-21 16:40:16 -0300

    Philosophers | ft_utils

--- Rank03/Philosophers/srcs/main.c
+++ Rank03/Philosophers/srcs/main.c
@@ -6,20 +6,11 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 16:36:45 by marcosv2          #+#    #+#             */
-/*   Updated: 2024/01/21 16:39:18 by marcosv2         ###   ########.fr       */
+/*   Updated: 2024/01/21 16:39:46 by marcosv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-static void	testing(void)
-{
-	int	i;
-
-	i = -1;
-	while (++i < 5)
-		ft_putstr(MSS_FORK);
-}
 
 static void	ph_init(t_info *info, int ac, char **av)
 {

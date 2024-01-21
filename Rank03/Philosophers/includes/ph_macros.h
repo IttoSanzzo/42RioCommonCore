d041099ddd9afb1048483e42c88d30966c4aebfe
IttoSanzzo <konohittos@gmail.com>
2024-01-21 16:20:03 -0300

    Philosophers | Macro Messages

--- Rank03/Philosophers/includes/ph_macros.h
+++ Rank03/Philosophers/includes/ph_macros.h
@@ -6,7 +6,7 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 22:01:56 by marcosv2          #+#    #+#             */
-/*   Updated: 2024/01/19 22:55:22 by marcosv2         ###   ########.fr       */
+/*   Updated: 2024/01/21 16:19:50 by marcosv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 # define PH_MACROS_H 
 
 // Macros
+
+// States
+# define MSS_FORK		" has taken a fork"
+# define MSS_EATING 	" is eating"
+# define MSS_SLEEPING	" is sleeping"
+# define MSS_THINKING	" is thinking"
+# define MSS_DIED		" died"
 
 #endif

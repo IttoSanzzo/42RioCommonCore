ff370375d0395f00ee21ac6b99614bc82c2a5aa9
IttoSanzzo <konohittos@gmail.com>
2024-01-21 16:36:00 -0300

    Philosophers | Macro Messages

--- Rank03/Philosophers/srcs/main.c
+++ Rank03/Philosophers/srcs/main.c
@@ -1,16 +1,32 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: marcosv2 <marcosv2@student.42.rio>         +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/19 23:16:35 by marcosv2          #+#    #+#             */
-/*   Updated: 2024/01/21 16:11:37 by marcosv2         ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   main.c											 :+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: marcosv2 <marcosv2@student.42.rio>		 +#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2024/01/19 23:16:35 by marcosv2		  #+#	#+#			 */
+/*   Updated: 2024/01/21 16:33:40 by marcosv2		 ###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int	 ft_atoi(const char *nptr)
+{
+	int		total;
+	int		signal;
+
+	total = 0;
+	signal = 1;
+	while ((*nptr >= '\t' && *nptr <= '\r') || *nptr == ' ')
+		nptr++;
+	if ((*nptr == '-' || *nptr == '+') && *nptr++ == '-')
+		signal *= -1;
+	while (*nptr >= '0' && *nptr <= '9')
+		total = total * 10 + (*nptr++ - '0');
+	return (total * signal);
+}
 
 void	ft_putstr(char *s)
 {
@@ -26,7 +42,7 @@ static void	testing(void)
 
 	i = -1;
 	while (++i < 5)
-		ft_putstr("bananada\n");
+		ft_putstr(MSS_FORK);
 }
 
 static void	ph_init(t_info *info, int ac, char **av)

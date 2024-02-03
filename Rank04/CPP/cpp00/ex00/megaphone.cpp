25ddf50a550af0747f5b403b26662f594db077c3
marcosv2 <konohittos@gmail.com>
2024-02-02 22:31:30 -0300

    cpp00 | ex00

--- Rank04/CPP/cpp00/ex00/megaphone.cpp
+++ Rank04/CPP/cpp00/ex00/megaphone.cpp
@@ -6,24 +6,18 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 15:11:49 by marcosv2          #+#    #+#             */
-/*   Updated: 2024/02/02 22:14:30 by marcosv2         ###   ########.fr       */
+/*   Updated: 2024/02/02 22:30:58 by marcosv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-static void	ft_uppercase(char *s) {
-	for (int i = 0; s[i]; i++)
-		if (s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= 32;
-	std::cout << s;
-}
-
 int	main(int ac, char **av) {
 	if (ac < 2)
 		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n" , 0);
 	for (int i = 1; i < ac; i++)
-		ft_uppercase(av[i]);
+		for (int y = 0; av[i][y]; y++)
+			std::cout << (char)std::toupper(av[i][y]);
 	std::cout << std::endl;
 	return (0);
 }

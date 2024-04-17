7dc18b957c53502c2d098b7ea0c53163c2d4cab7
IttoSanzzo <konohittos@gmail.com>
2024-04-17 09:38:13 -0300

    CPP04 | ex02 WrongCat & Brain fixes

--- Rank04/CPP/cpp04/ex02/Brain.cpp
+++ Rank04/CPP/cpp04/ex02/Brain.cpp
@@ -6,23 +6,26 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 17:52:09 by marcosv2          #+#    #+#             */
-/*   Updated: 2024/04/11 18:30:05 by marcosv2         ###   ########.fr       */
+/*   Updated: 2024/04/17 09:26:11 by marcosv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Brain.hpp"
 
 Brain::~Brain(void) {
+	std::cout << "Brain default destructor called!" << std::endl;
 }
 
 Brain::Brain(void) {
 	for (int i = 0; i < 100; i++)
 		this->ideas[i] = "Spaghetti ideas";
+	std::cout << "Brain default constructor called!" << std::endl;
 }
 
 Brain::Brain(const Brain& src) {
 	for(int i = 0; i < 100; i++)
 		this->ideas[i] = src.ideas[i];
+	std::cout << "Brain copy constructor called!" << std::endl;
 }
 
 Brain&	Brain::operator=(const Brain& src) {
--- Rank04/CPP/cpp04/ex02/WrongCat.cpp
+++ Rank04/CPP/cpp04/ex02/WrongCat.cpp
@@ -6,7 +6,7 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 17:28:52 by marcosv2          #+#    #+#             */
-/*   Updated: 2024/04/11 17:34:28 by marcosv2         ###   ########.fr       */
+/*   Updated: 2024/04/17 09:10:24 by marcosv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ WrongCat::~WrongCat(void) {
 }
 
 WrongCat::WrongCat(void) {
-	this->type = "wrongcat";
+	this->type = "WrongCat";
 	std::cout << "WrongCat default constructor called!" << std::endl;
 }
 

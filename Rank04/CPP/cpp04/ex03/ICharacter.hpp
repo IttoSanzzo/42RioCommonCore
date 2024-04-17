d6ef82ae80819f2aa2080ab34ffcda2c630d01d5
IttoSanzzo <konohittos@gmail.com>
2024-04-17 09:55:48 -0300

    CPP04 | ex03 Useless Interface cpp removals

--- Rank04/CPP/cpp04/ex03/ICharacter.hpp
+++ Rank04/CPP/cpp04/ex03/ICharacter.hpp
@@ -6,7 +6,7 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 19:58:29 by marcosv2          #+#    #+#             */
-/*   Updated: 2024/04/12 01:22:42 by marcosv2         ###   ########.fr       */
+/*   Updated: 2024/04/17 09:54:27 by marcosv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ class	AMateria;
 
 class ICharacter {
 	public:
-		virtual ~ICharacter(void);
+		virtual ~ICharacter(void) {}
 		virtual std::string const	&getName(void) = 0;
 		virtual void	equip(AMateria* mat) = 0;
 		virtual void	unequip(int	index) = 0;

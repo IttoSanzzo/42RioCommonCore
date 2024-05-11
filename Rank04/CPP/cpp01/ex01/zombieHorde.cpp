dcf8db7a94d2d922abe50c46711caababeae965f
IttoSanzzo <konohittos@gmail.com>
2024-05-10 21:20:33 -0300

    CPP01 | ex01 | Horde (void)name

--- Rank04/CPP/cpp01/ex01/zombieHorde.cpp
+++ Rank04/CPP/cpp01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: marcosv2 <marcosv2@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 21:33:00 by marcosv2          #+#    #+#             */
-/*   Updated: 2024/04/04 22:09:59 by marcosv2         ###   ########.fr       */
+/*   Updated: 2024/05/10 21:19:47 by marcosv2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 Zombie*	zombieHorde(int N, std::string name) {
 	Zombie	*zh = new Zombie[N];
 
-(void)name;
 	for (int i = 0; i < N; i++) {
 		zh[i].setName(name);
 	}
